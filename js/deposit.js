6811b6afcc927ceb78159c1b0736ae32fa812208
Fatema-Khanom <fatemaurmi2019@gmail.com>
2023-08-18 01:29:18 +0600

    all-done

--- js/deposit.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btn-deposit').addEventListener('click', function(){
-    const depositField=document.getElementById('deposit-field');
-    const depositAmount=depositField.value ;
-    const
-})
